f6ff31c166739a51238ff0040d1cc44482461428
test1 <alishevakamalia@gmail.com>
2026-02-16 18:03:41 +0200

    Updated Second Task

--- Practice02/src/task02/setup.py
+++ Practice02/src/task02/setup.py
@@ -7,7 +7,8 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/task02.launch']),
         ('share/' + package_name + '/config', ['config/task02.yaml']),
@@ -16,7 +17,7 @@ setup(
     zip_safe=True,
     maintainer='user',
     maintainer_email='user@example.com',
-    description='Task 2: publisher node that publishes String messages to topic from config; text from CLI param.',
+    description='Task 2: publisher node',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
